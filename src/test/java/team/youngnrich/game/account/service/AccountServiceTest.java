939ba8721a9945576ea069214a249be4480fa53b
Haeun Noh <00blowup@gmail.com>
2024-06-13 03:09:54 +0900

    test: Mock AccountRepository를 사용하도록 수정

--- src/test/java/team/youngnrich/game/account/service/AccountServiceTest.java
+++ src/test/java/team/youngnrich/game/account/service/AccountServiceTest.java
@@ -2,14 +2,25 @@ package team.youngnrich.game.account.service;
 
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import team.youngnrich.game.account.domain.Account;
 import team.youngnrich.game.account.dto.request.SignupRequestDto;
 import team.youngnrich.game.account.dto.response.SignupResponseDto;
+import team.youngnrich.game.account.repository.AccountRepository;
 
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
+
+    @Mock
+    AccountRepository accountRepository;
 
     @Test
     void signup_테스트_정상_응답값확인() {
@@ -19,7 +30,13 @@ class AccountServiceTest {
         SignupRequestDto REQUEST_DTO = SignupRequestDto.builder()
                 .code(CODE_PROVIDED_BY_CLIENT)
                 .build();
-        AccountService accountService = new AccountService();
+        Account account = Account.builder()
+                .kakaoId("12345678")
+                .nickname("nickname")
+                .profileImageUrl("example.com")
+                .build();
+        when(accountRepository.save(any(Account.class))).thenReturn(account);
+        AccountService accountService = new AccountService(accountRepository);
         // when
         SignupResponseDto responseDto = accountService.signup(REQUEST_DTO);
         // then
@@ -40,7 +57,13 @@ class AccountServiceTest {
         SignupRequestDto REQUEST_DTO = SignupRequestDto.builder()
                 .code(WRONG_AUTHORIZATION_CODE)
                 .build();
-        AccountService accountService = new AccountService();
+        Account account = Account.builder()
+                .kakaoId("12345678")
+                .nickname("nickname")
+                .profileImageUrl("example.com")
+                .build();
+        when(accountRepository.save(any(Account.class))).thenReturn(account);
+        AccountService accountService = new AccountService(accountRepository);
         // when, then
         assertThrows(IllegalArgumentException.class, () -> accountService.signup(REQUEST_DTO));
     }
