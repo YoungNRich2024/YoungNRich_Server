b4524d10280e8204a964dea05374187f5826f197
Haeun Noh <00blowup@gmail.com>
2024-06-13 02:55:57 +0900

    feat-회원관리: Account 도메인 작성 및 테스트 완료

--- src/main/java/team/youngnrich/game/account/domain/Account.java
+++ src/main/java/team/youngnrich/game/account/domain/Account.java
@@ -1,4 +1,73 @@
 package team.youngnrich.game.account.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Account {
+
+    @Transient
+    private static Long NICKNAME_MAX_LENGTH;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id", nullable = false, updatable = false)
+    private Long accountId;
+
+    @Column(nullable = false)
+    private String kakaoId;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String profileImageUrl;
+
+    @Builder
+    public Account(String kakaoId, String nickname, String profileImageUrl) {
+        validateKakaoId(kakaoId);
+        validateNickname(nickname);
+        this.kakaoId = kakaoId;
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public void updateNickname(String newNickname) {
+        validateNickname(newNickname);
+        this.nickname = newNickname;
+    }
+
+    public void updateProfileImageUrl(String newUrl) {
+        this.profileImageUrl = newUrl;
+    }
+
+    private void validateKakaoId(String kakaoId) {
+        try{
+            Integer.parseInt(kakaoId);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 카카오회원 ID가 정수가 아닙니다!");
+        }
+    }
+
+    private void validateNickname(String nickname) {
+        if(nickname.length()> NICKNAME_MAX_LENGTH)
+            throw new IllegalArgumentException("[ERROR] 닉네임이 너무 깁니다!");
+    }
+
+    @Component
+    public static class AccountPropertyInitializer {
+        @Autowired
+        public AccountPropertyInitializer(Environment environment) {
+            NICKNAME_MAX_LENGTH = Long.parseLong(environment.getProperty("numbers.account.nickname-max"));
+        }
+    }
 }
