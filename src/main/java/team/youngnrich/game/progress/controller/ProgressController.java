0a366c4453c4eabd550823661f86656b61516f8f
Haeun Noh <00blowup@gmail.com>
2024-06-13 21:59:43 +0900

    refactor: 컨트롤러가 서비스에 넘겨주는 값을 Authentication에서 사용자의 kakaoId로 변경

--- src/main/java/team/youngnrich/game/progress/controller/ProgressController.java
+++ src/main/java/team/youngnrich/game/progress/controller/ProgressController.java
@@ -14,41 +14,41 @@ public class ProgressController {
 
     @GetMapping("/exist")
     public ProgressExistResponseDto checkIfExists (Authentication authentication) {
-        return progressService.checkIfExists(authentication);
+        return progressService.checkIfExists(authentication.getName());
     }
 
     @GetMapping
     public ProgressResponseDto getMyProgress (Authentication authentication) {
-        return progressService.getMyProgress(authentication);
+        return progressService.getMyProgress(authentication.getName());
     }
 
     @PostMapping("/puzzle")
     public ResponseEntity<String> puzzleSolved (Authentication authentication, PuzzleSolvedRequestDto requestDto) {
-        return progressService.puzzleSolved(authentication, requestDto);
+        return progressService.puzzleSolved(authentication.getName(), requestDto);
     }
 
     @PostMapping("/keyget")
     public ResponseEntity<String> getKey (Authentication authentication) {
-        return progressService.getKey(authentication);
+        return progressService.getKey(authentication.getName());
     }
 
     @PostMapping("/keyuse")
     public ResponseEntity<String> useKey (Authentication authentication) {
-        return progressService.useKey(authentication);
+        return progressService.useKey(authentication.getName());
     }
 
     @PostMapping("/test")
     public ResponseEntity<String> test (Authentication authentication, @RequestBody TestRequestDto requestDto) {
-        return progressService.test(authentication, requestDto);
+        return progressService.test(authentication.getName(), requestDto);
     }
 
     @PostMapping("/new")
     public ResponseEntity<String> newGame (Authentication authentication) {
-        return progressService.newGame(authentication);
+        return progressService.newGame(authentication.getName());
     }
 
     @PostMapping("/save")
     public ResponseEntity<String> save (Authentication authentication, SaveRequestDto requestDto) {
-        return progressService.save(authentication, requestDto);
+        return progressService.save(authentication.getName(), requestDto);
     }
 }
