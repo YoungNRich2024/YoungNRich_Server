e116e3eaed6be810765bf4cb7683195da48ea09d
Haeun Noh <00blowup@gmail.com>
2024-06-13 17:46:12 +0900

    test: 지난 커밋에 완성된 KakaoLoginService mock 객체를 테스트 코드에 추가

--- src/test/java/team/youngnrich/game/account/service/AccountServiceTest.java
+++ src/test/java/team/youngnrich/game/account/service/AccountServiceTest.java
@@ -1,6 +1,7 @@
 package team.youngnrich.game.account.service;
 
 import org.assertj.core.api.SoftAssertions;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -9,6 +10,8 @@ import team.youngnrich.game.account.domain.Account;
 import team.youngnrich.game.account.dto.request.SignupRequestDto;
 import team.youngnrich.game.account.dto.response.SignupResponseDto;
 import team.youngnrich.game.account.repository.AccountRepository;
+import team.youngnrich.game.oauth.dto.KakaoUserInfoResponseDto;
+import team.youngnrich.game.oauth.service.KakaoLoginService;
 
 import java.util.Scanner;
 
@@ -22,8 +25,11 @@ class AccountServiceTest {
     @Mock
     AccountRepository accountRepository;
 
+    @Mock
+    KakaoLoginService kakaoLoginService;
+
     @Test
-    void signup_테스트_오류확인_클라이언트가_잘못된_인가코드를_전달한_경우() {
+    void signup_테스트_오류확인_클라이언트가_잘못된_인가코드를_전달한_경우() throws ParseException {
         // given
         String WRONG_AUTHORIZATION_CODE = "abcd";
         SignupRequestDto REQUEST_DTO = SignupRequestDto.builder()
@@ -35,7 +41,13 @@ class AccountServiceTest {
                 .profileImageUrl("example.com")
                 .build();
         when(accountRepository.save(any(Account.class))).thenReturn(account);
-        AccountService accountService = new AccountService(accountRepository);
+        KakaoUserInfoResponseDto infoResponseDto = KakaoUserInfoResponseDto.builder()
+                .kakaoId("12345678")
+                .nickname("nickname")
+                .profileImageUrl("example.com")
+                .build();
+        when(kakaoLoginService.login(any(String.class))).thenReturn(infoResponseDto);
+        AccountService accountService = new AccountService(accountRepository, kakaoLoginService);
         // when, then
         assertThrows(IllegalArgumentException.class, () -> accountService.signup(REQUEST_DTO));
     }
