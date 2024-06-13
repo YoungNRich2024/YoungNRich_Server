d3648427826574c01330ea7c2ba46d34d32bc6cd
Haeun Noh <00blowup@gmail.com>
2024-06-13 21:52:30 +0900

    feat-중간저장: 컨트롤러 설계 1차 완료

---
+++ src/main/java/team/youngnrich/game/progress/controller/ProgressController.java
@@ -0,0 +1,69 @@
+package team.youngnrich.game.progress.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import team.youngnrich.game.progress.service.ProgressService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/progress")
+public class ProgressController {
+    private final ProgressService progressService;
+
+    @GetMapping("/exist")
+    public ProgressExistResponseDto checkIfExists (Authentication authentication) {
+        return progressService.checkIfExists(authentication);
+    }
+
+    @GetMapping
+    public ProgressResponseDto getMyProgress (Authentication authentication) {
+        return progressService.getMyProgress(authentication);
+    }
+
+    @PostMapping("/pone")
+    public ResponseEntity<String> puzzleOne (Authentication authentication) {
+        return progressService.puzzleOne(authentication);
+    }
+
+    @PostMapping("/ptwo")
+    public ResponseEntity<String> puzzleTwo (Authentication authentication) {
+        return progressService.puzzleTwo(authentication);
+    }
+
+    @PostMapping("/pthree")
+    public ResponseEntity<String> puzzleThree (Authentication authentication) {
+        return progressService.puzzleThree(authentication);
+    }
+
+    @PostMapping("/pfour")
+    public ResponseEntity<String> puzzleFour (Authentication authentication) {
+        return progressService.puzzleFour(authentication);
+    }
+
+    @PostMapping("/keyget")
+    public ResponseEntity<String> getKey (Authentication authentication) {
+        return progressService.getKey(authentication);
+    }
+
+    @PostMapping("/keyuse")
+    public ResponseEntity<String> useKey (Authentication authentication) {
+        return progressService.useKey(authentication);
+    }
+
+    @PostMapping("/test")
+    public ResponseEntity<String> test (Authentication authentication, @RequestBody TestRequestDto requestDto) {
+        return progressService.getKey(authentication, requestDto);
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<String> newGame (Authentication authentication) {
+        return progressService.newGame(authentication);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<String> save (Authentication authentication, SaveRequestDto requestDto) {
+        return progressService.save(authentication, requestDto);
+    }
+}
