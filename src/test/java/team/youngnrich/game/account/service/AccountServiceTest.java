fa2a7447946f4a06ca67699f34534fdaba73fea5
Haeun Noh <00blowup@gmail.com>
2024-06-13 17:40:20 +0900

    test: 매번 올바른 인가코드를 직접 입력해야 하므로 정상 테스트 코드 삭제, Postman 테스트로 대체하기로 함

--- src/test/java/team/youngnrich/game/account/service/AccountServiceTest.java
+++ src/test/java/team/youngnrich/game/account/service/AccountServiceTest.java
@@ -23,35 +23,6 @@ class AccountServiceTest {
     AccountRepository accountRepository;
 
     @Test
-    void signup_테스트_정상_응답값확인() {
-        // given
-        Scanner sc = new Scanner(System.in);
-        String CODE_PROVIDED_BY_CLIENT = sc.nextLine();
-        SignupRequestDto REQUEST_DTO = SignupRequestDto.builder()
-                .code(CODE_PROVIDED_BY_CLIENT)
-                .build();
-        Account account = Account.builder()
-                .kakaoId("12345678")
-                .nickname("nickname")
-                .profileImageUrl("example.com")
-                .build();
-        when(accountRepository.save(any(Account.class))).thenReturn(account);
-        AccountService accountService = new AccountService(accountRepository);
-        // when
-        SignupResponseDto responseDto = accountService.signup(REQUEST_DTO);
-        // then
-        SoftAssertions softly = new SoftAssertions();
-        softly.assertThat(responseDto.getId()).isNotNull();
-        softly.assertThat(responseDto.getKakaoId()).isNotNull();
-        softly.assertThat(responseDto.getNickname()).isNotNull();
-        softly.assertThat(responseDto.getProfileImageUrl()).isNotNull();
-        softly.assertThat(responseDto.getAccessToken()).isNotNull();
-        softly.assertThat(responseDto.getRefreshToken()).isNotNull();
-        softly.assertAll();
-        sc.close();
-    }
-
-    @Test
     void signup_테스트_오류확인_클라이언트가_잘못된_인가코드를_전달한_경우() {
         // given
         String WRONG_AUTHORIZATION_CODE = "abcd";
