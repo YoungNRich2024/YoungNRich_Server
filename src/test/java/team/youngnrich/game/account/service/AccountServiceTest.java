9a906b1e10a7eac5572287f9a4402c36bd10b105
Haeun Noh <00blowup@gmail.com>
2024-06-14 16:54:16 +0900

    refactor: 필요 없는 import문 삭제

--- src/test/java/team/youngnrich/game/account/service/AccountServiceTest.java
+++ src/test/java/team/youngnrich/game/account/service/AccountServiceTest.java
@@ -1,6 +1,5 @@
 package team.youngnrich.game.account.service;
 
-import org.assertj.core.api.SoftAssertions;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,13 +9,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import team.youngnrich.game.account.domain.Account;
 import team.youngnrich.game.account.dto.request.SignupRequestDto;
-import team.youngnrich.game.account.dto.response.SignupResponseDto;
 import team.youngnrich.game.account.repository.AccountRepository;
 import team.youngnrich.game.oauth.dto.KakaoUserInfoResponseDto;
 import team.youngnrich.game.oauth.service.KakaoLoginService;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
